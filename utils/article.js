58402ffce50752798374573a6b4df72c0c6e0971
wlyq1357924680 <1925321054@qq.com>
2019-12-30 18:14:28 +0800

    完成了发表文章的发布功能

--- utils/article.js
+++ utils/article.js
@@ -3,5 +3,18 @@ var article={
         $.get(baseUrl+'/admin/search',data,function(res){
             callback(res)
         })
+    },
+    add:function(fd,callback){
+        $.ajax({
+            type:'POST',
+            url:baseUrl+'/admin/article_publish',
+            data:fd,
+            success:function(res){
+                callback(res)
+            },
+            
+            processData:false,
+            contentType:false
+        })
     }
 }
