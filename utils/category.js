1a7aa320204ebbbde909b5cb1362aa730e76ef95
wlyq1357924680 <1925321054@qq.com>
2019-12-30 11:43:38 +0800

    测试了一下

--- utils/category.js
+++ utils/category.js
@@ -8,14 +8,12 @@ var caterage={
     delete:function(id,callback){
         $.post(baseUrl+'/admin/category_delete',{id:id},function(res){
             callback(res)
-
         })
     },
     add:function(name,slug,callback){
         $.post(baseUrl+'/admin/category_add',{name:name,slug:slug},function(res){
             callback(res)
         })
-
     },
     updata:function(id,name,slug,callback){
         $.post(baseUrl+'/admin/category_edit',{id:id,name:name,slug:slug},function(res){
